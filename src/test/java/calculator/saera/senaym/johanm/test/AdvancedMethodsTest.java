93ee3b6b9ec09349b957be2025c9211e5ea8bd95
SaerAlyasso <saer.alyasso@yh.nackademin.se>
2016-11-07 12:32:21 +0100

    Round method done

--- src/test/java/calculator/saera/senaym/johanm/test/AdvancedMethodsTest.java
+++ src/test/java/calculator/saera/senaym/johanm/test/AdvancedMethodsTest.java
@@ -14,12 +14,20 @@ public class AdvancedMethodsTest {
 	AdvancedMethods am = new AdvancedMethods();
 	Random rand = new Random();
 	
-	public static final Logger log = Logger.getLogger(AdvancedMethods.class.getName());
+	public static final Logger LOG = Logger.getLogger(AdvancedMethods.class.getName());
 	
 	@Test
 	public void testRoundNumber() {
 		
 		
+		
+		for(int i=0; i<50; i++){
+			double number = rand.nextDouble()*100;
+			double result = Math.round(number);
+			
+			LOG.info("Testing the method roundNumber with the value "+number);
+			assertEquals("The result should be: "+ result,am.roundNumber(number),result,0.0);
+		}
 		
 	}
 
