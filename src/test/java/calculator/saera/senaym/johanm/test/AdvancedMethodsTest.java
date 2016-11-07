1a18310bfeaf56f4557df61326da5167d9bcb3a1
SaerAlyasso <saer.alyasso@yh.nackademin.se>
2016-11-07 11:59:07 +0100

    working with testcases

--- src/test/java/calculator/saera/senaym/johanm/test/AdvancedMethodsTest.java
+++ src/test/java/calculator/saera/senaym/johanm/test/AdvancedMethodsTest.java
@@ -2,13 +2,25 @@ package calculator.saera.senaym.johanm.test;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+import java.util.logging.Logger;
+
 import org.junit.Test;
+
+import calculator.git.otherclasses.AdvancedMethods;
 
 public class AdvancedMethodsTest {
 
+	AdvancedMethods am = new AdvancedMethods();
+	Random rand = new Random();
+	
+	public static final Logger log = Logger.getLogger(AdvancedMethods.class.getName());
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRoundNumber() {
+		
+		
+		
 	}
 
 }
