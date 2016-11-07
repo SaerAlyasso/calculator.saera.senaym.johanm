0ec4ff1323c7ea208c70b15bb83ecf07844fce4e
Johan Moback <Johan Moback>
2016-11-07 09:19:35 +0100

    try, but failed to add action to my buttions

--- src/main/java/calculator/git/otherclasses/ViewClass.java
+++ src/main/java/calculator/git/otherclasses/ViewClass.java
@@ -25,6 +25,13 @@ public class ViewClass implements ActionListener{
 	private JFrame frame;
 	private JTextField textField = new JTextField();
 	
+	double firstnum;
+	double secondnum;
+	double result; 
+	String operations; 
+	String answer;
+	int switchStatement; 
+	
 	// First row
 	private JButton btnClear = new JButton("C");
 	private JButton btnFibonacci = new JButton("Fn");
@@ -228,7 +235,121 @@ public class ViewClass implements ActionListener{
 	 * event and its source.
 	 */
 	public void actionPerformed(ActionEvent e) {
+		BasicMethods bm = new BasicMethods(); 
 		
+		if(e.getSource()==btn0){
+			firstnum=Double.parseDouble(btn0.getText());
+			textField.setText(""+firstnum);
+			textField.setText(""); 
+			secondnum = Double.parseDouble(btn0.getText()); 
+			textField.setText(""+secondnum);   
+		}
+		
+		if(e.getSource()==btn1){//btnEqual---> 
+			firstnum=Double.parseDouble(btn1.getText());
+			textField.setText(""+firstnum);
+			textField.setText(""); 
+			secondnum = Double.parseDouble(btn1.getText()); 
+			textField.setText(""+secondnum);   
+		
+		}// btn1<---
+		if(e.getSource()==btn2){
+			firstnum=Double.parseDouble(btn2.getText());
+			textField.setText(""+firstnum); 
+		}
+		
+		if(e.getSource()==btn3){
+			firstnum=Double.parseDouble(btn3.getText());
+			textField.setText(""+firstnum); 
+		}
+		
+		if(e.getSource()==btn4){
+			firstnum=Double.parseDouble(btn4.getText());
+			textField.setText(""+firstnum); 
+		}
+		
+		if(e.getSource()==btn5){
+			firstnum=Double.parseDouble(btn5.getText());
+			textField.setText(""+firstnum); 
+		}
+		
+		if(e.getSource()==btn6){
+			firstnum=Double.parseDouble(btn6.getText());
+			textField.setText(""+firstnum); 
+		}
+		
+		if(e.getSource()==btn7){
+			firstnum=Double.parseDouble(btn7.getText());
+			textField.setText(""+firstnum); 
+		}
+		
+		if(e.getSource()==btn8){
+			firstnum=Double.parseDouble(btn8.getText());
+			textField.setText(textField.getText()+firstnum); 
+		}
+		
+		if(e.getSource()==btn9){
+			firstnum=Double.parseDouble(btn9.getText());
+			textField.setText(""+firstnum); 
+		}
+		
+		if(e.getSource()==btnAddition){
+			textField.setText("");
+			operations = "+"; 
+		}
+		
+		if(e.getSource()==btnSubtraction){
+			textField.setText("");
+			operations="-";
+		}
+		
+		if(e.getSource()==btnEqual){
+				 
+				secondnum= Double.parseDouble(textField.getText());
+				
+				result = bm.addition(firstnum, secondnum);
+				answer = String.format("%2B", result); 
+				textField.setText(answer);    
+		}
+		switchStatement = 4; 
+		
+		switch (switchStatement) {
+			case 1: {
+				secondnum= Double.parseDouble(textField.getText());
+				
+				result = bm.addition(firstnum, secondnum);
+				answer = String.format("%2f", result); 
+				textField.setText(answer);    
+				
+			}
+			case 2: {
+				textField.setText(""); 
+				secondnum = Double.parseDouble(textField.getText()); 
+				
+				result = bm.subtraction(firstnum, secondnum);  
+				answer = String.format("%2f", result); 
+				textField.setText(answer);
+			}
+			case 3: {
+				textField.setText(""); 
+				secondnum = Double.parseDouble(textField.getText()); 
+				
+				result = bm.multiplication(firstnum, secondnum); 
+				answer = String.format("%2f", result); 
+				textField.setText(answer); 
+			}
+		/*	case 4: {
+				textField.setText(""); 
+				secondnum = Double.parseDouble(textField.getText()); 
+				
+				result = bm.division(firstnum, secondnum); 
+				answer = String.format("%2f", result); 
+				textField.setText(answer);
+			}*/ 
+		}
+
+	
 		
 	}
-}
+} 
+
