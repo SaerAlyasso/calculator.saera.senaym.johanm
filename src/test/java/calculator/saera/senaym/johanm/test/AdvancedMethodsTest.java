b37ce8b07a5ff6cafdf770bb768f3a2e18a0c7dc
SaerAlyasso <saer.alyasso@yh.nackademin.se>
2016-11-07 13:39:28 +0100

    Advanced methods done and two dependencies added to pom.xml

--- src/test/java/calculator/saera/senaym/johanm/test/AdvancedMethodsTest.java
+++ src/test/java/calculator/saera/senaym/johanm/test/AdvancedMethodsTest.java
@@ -44,5 +44,68 @@ public class AdvancedMethodsTest {
 			assertEquals("The result should be: "+ result,am.roundNumber(number),result,0.0);
 		}
 	}
+	
+	@Test
+	public void testCountPow(){
+		
+		double firstNumber=0.0;
+		double secondNumber=0.0;
+		double result=0.0;
+		
+	
+		for(int i=0; i<50; i++){
+			firstNumber = rand.nextDouble()*100;
+			secondNumber = rand.nextDouble()*100;
+			result = Math.pow(firstNumber, secondNumber);
+			
+			LOG.info("Testing the method countPow with the value "+firstNumber+" and "+ secondNumber);
+			assertEquals("The result should be: "+ result,am.countPow(firstNumber, secondNumber),result,0.0);
+		}
+	}
+	
+	@Test
+	public void testCountAbsolute(){
+		
+		double number=0.0;
+		double result=0.0;
+	
+		for(int i=0; i<50; i++){
+			number = rand.nextDouble()*100;
+			result = Math.abs(number);
+			
+			LOG.info("Testing the method countAbsolute with the value "+number);
+			assertEquals("The result should be: "+ result,am.countAbsolute(number),result,0.0);
+		}
+	}
+	
+	@Test
+	public void testCountSin(){
+		
+		double number=0.0;
+		double result=0.0;
+		
+		for(int i=0; i<50; i++){
+			number = rand.nextDouble()*100;
+			result = Math.sin(number);
+			
+			LOG.info("Testing the method countSin with the value "+number);
+			assertEquals("The result should be: "+ result,am.countSin(number),result,0.0);
+		}
+	}
+	
+	@Test
+	public void testCountSquare(){
+		
+		double number=0.0;
+		double result=0.0;
+		
+		for(int i=0; i<50; i++){
+			number = rand.nextDouble()*100;
+			result = Math.sqrt(number);
+			
+			LOG.info("Testing the method countSquare with the value "+number);
+			assertEquals("The result should be: "+ result,am.countSquare(number),result,0.0);
+		}
+	}
 
 }
